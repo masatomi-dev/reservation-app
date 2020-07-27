ac7953ace493199c66a10c3c96f76ba6b6781f13
“Masatomi-dev” <go1masatomi@gmail.com>
2020-07-28 01:10:43 +0900

    Angularフロントエンド側作成

---
+++ src/app/products.ts
@@ -0,0 +1,50 @@
+export const products = [
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone XL',
+        price:799,
+        description:'A large phone with one of the best screens',
+        heading1: 'サンプルテキスト１',
+        heading2: 'サンプルテキスト２',
+        heading3: 'サンプルテキスト３',
+        headingtext1: 'ベストの携帯電話でっせベストの携帯電話でっせベストの携帯電話でっせベストの携帯電話でっせ',
+        headingtext2: 'ほんまにすごいんでっせほんまにすごいんでっせほんまにすごいんでっせほんまにすごいんでっせ',
+        headingtext3: '絶対買ってください絶対買ってください絶対買ってください絶対買ってください絶対買ってください'
+    },
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Mini',
+        price:699,
+        description:'A great phone with one of the best cameras',
+        heading1: 'サンプルテキスト１',
+        heading2: 'サンプルテキスト２',
+        heading3: 'サンプルテキスト３',
+        headingtext1: 'ベストの携帯電話でっせベストの携帯電話でっせベストの携帯電話でっせベストの携帯電話でっせ',
+        headingtext2: 'ほんまにすごいんでっせほんまにすごいんでっせほんまにすごいんでっせほんまにすごいんでっせ',
+        headingtext3: '絶対買ってください絶対買ってください絶対買ってください絶対買ってください絶対買ってください'
+    },
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Standard',
+        price:299,
+        description:'',
+        heading1: 'サンプルテキスト１',
+        heading2: 'サンプルテキスト２',
+        heading3: 'サンプルテキスト３',
+        headingtext1: 'ベストの携帯電話でっせベストの携帯電話でっせベストの携帯電話でっせベストの携帯電話でっせ',
+        headingtext2: 'ほんまにすごいんでっせほんまにすごいんでっせほんまにすごいんでっせほんまにすごいんでっせ',
+        headingtext3: '絶対買ってください絶対買ってください絶対買ってください絶対買ってください絶対買ってください'
+    },
+    {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Special',
+        price:999,
+        description:'',
+        heading1: 'サンプルテキスト１',
+        heading2: 'サンプルテキスト２',
+        heading3: 'サンプルテキスト３',
+        headingtext1: 'ベストの携帯電話でっせベストの携帯電話でっせベストの携帯電話でっせベストの携帯電話でっせ',
+        headingtext2: 'ほんまにすごいんでっせほんまにすごいんでっせほんまにすごいんでっせほんまにすごいんでっせ',
+        headingtext3: '絶対買ってください絶対買ってください絶対買ってください絶対買ってください絶対買ってください'
+    }
+];
